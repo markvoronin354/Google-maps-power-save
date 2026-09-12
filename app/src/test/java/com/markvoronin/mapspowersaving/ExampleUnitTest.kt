2abfc482266bc4d7602fe8ad61c3d82d3a4a9357
markvoronin354 <markvoronin354@gmail.com>
2026-09-12 08:30:10 -0400

    cleanning

---
+++ app/src/test/java/com/markvoronin/mapspowersaving/ExampleUnitTest.kt
@@ -0,0 +1,14 @@
+package com.markvoronin.mapspowersaving
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test for maps power save.
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
